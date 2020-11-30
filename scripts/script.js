43cf40a14045635a17641931f0e4f21e4f204f97
Igor Skorokhodov <i.vlad.sk@gmail.com>
2020-11-30 13:20:52 +0700

    Вторая итерация

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+let submitButton = document.querySelector('.popup__submit-button');
+let name = document.querySelector('.profile__name');
+let profession = document.querySelector('.profile__profession');
+let popUp = document.querySelector('.popup');
+let placeHolderName = name.textContent;
+let placeHolderProfession = profession.textContent;
+let formElementName = document.querySelector('.popup__form_name');
+let formElementJob = document.querySelector('.popup__form_job');
+console.log(name.textContent);
+
+
+function addPopup() {
+   popUp.classList.add('popup_active');
+   formElementName.value = name.textContent;
+   formElementJob.value = profession.textContent;
+}
+
+function closePopup() {
+    popUp.classList.remove('popup_active');
+ }
+
+ function editInfo () {
+    function formSubmitHandler (evt) {
+        evt.preventDefault();
+    }
+    let nameInput = formElementName.value;
+    let jobInput = formElementJob.value;
+    name.textContent=nameInput;
+    profession.textContent=jobInput;
+    closePopup();
+ }
+ 
+ editButton.addEventListener('click', addPopup); 
+ closeButton.addEventListener('click', closePopup);
+ submitButton.addEventListener('click', editInfo);
+
+
+
+
+
+
